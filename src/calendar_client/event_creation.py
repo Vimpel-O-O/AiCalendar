69d81098d16bd01a985acc9f8dfd6a09163d7e80
Shamil <shamil.300085@gmail.com>
2025-05-06 15:33:45 -0700

    fix: added google user token update thru oauth manual login

--- src/calendar_client/event_creation.py
+++ src/calendar_client/event_creation.py
@@ -2,30 +2,48 @@ from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from config import GOOGLE_CREDENTIALS, GOOGLE_USER_TOKEN
+from google.auth.transport.requests import Request
+from dotenv import set_key
+import os
+import sys
+import google.auth.exceptions
 
 class CalendarClient:
     def __init__(self):
         # access scope for calendar API
         self.SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-    def get_credentials(self):
-        try:
-          # get saved google account credentials from token.json file
-          creds = Credentials.from_authorized_user_info(GOOGLE_USER_TOKEN, self.SCOPES)
+    def restart_program(self):
+        """Restarts the current Python program."""
+        os.execv(sys.executable, [sys.executable] + sys.argv)
 
-        except FileNotFoundError:
-            # To generate token.json file with user credentials for the 1st time
-            flow = InstalledAppFlow.from_client_config(GOOGLE_CREDENTIALS, self.SCOPES)
+    def get_credentials(self):
+        # get saved google account credentials from token.json file
+        creds = Credentials.from_authorized_user_info(GOOGLE_USER_TOKEN, self.SCOPES)
+        
+        if creds.expired:
+            # User goes through browser login
+            flow = InstalledAppFlow.from_client_config(
+                GOOGLE_CREDENTIALS,
+                self.SCOPES
+            )
             creds = flow.run_local_server(port=0)
 
-            with open("token.json", "w") as token:
-               token.write(creds.to_json())
+            # Save new token to .env
+            set_key(".env", "GOOGLE_USER_TOKEN", creds.to_json())
+            self.restart_program()
         
         return creds
 
     def create_event(self, calID, event):
         service = build('calendar', 'v3', credentials=self.get_credentials())
-        event = service.events().insert(calendarId=calID, body=event).execute()
+        try:
+            event = service.events().insert(calendarId=calID, body=event).execute()
+        except google.auth.exceptions.RefreshError:
+            creds = Credentials.from_authorized_user_info(GOOGLE_USER_TOKEN, self.SCOPES)
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            event = service.events().insert(calendarId=calID, body=event).execute()
         
         return "Event succesfully created: " + event.get('htmlLink')
     
